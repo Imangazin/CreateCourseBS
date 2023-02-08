a31eb8745c61c3718451213b267531097cd038e8
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-02-08 13:07:47 -0500

    Add files via upload

--- createSite.php
+++ createSite.php
@@ -3,7 +3,7 @@ require_once("info.php");
 require_once 'lib/D2LAppContextFactory.php';
 require_once 'doValenceRequest.php';
 
-//read LTI tool Key and OrgUnitID passed by session from main page index.php
+//read LTI tool Key, Role and Username passed by session from main page index.php
 
 session_start();
 $toolKey = $_SESSION['toolKey'];
@@ -11,25 +11,58 @@ $roleId = $_SESSION['RoleId'];
 $userName = $_SESSION['UserName'];
 session_write_close();
 
-//Check the key is correct / wrap everything with LTI credentials
+//Check the LTI key is correct and role of the user in organization
 if(($lti_auth['key'] == $toolKey) && ($roleId == "faculty-staff" || $roleId == "Super Administrator")){
+    //recieve the site title from the user
     $siteName = htmlspecialchars($_POST['siteName']);
-    $explorerSite = htmlspecialchars($_POST['explorerSite']);
+    //remove special chars from the offering code
+    $siteCode = str_replace(array('\\',':','*','?','"','<','>','|','\'','#',',','%','&'),'',$siteName);
+    //default the course template to specific template 
+    $siteTemplate = config['project_site_id'];
+    //change template
+    if($_POST['explorerSite'] == 'yes') 
+        $siteTemplate = config['explorer_site_id'];
+    //recive a term
     $siteTerm = htmlspecialchars($_POST['siteTerm']);
     
-    //getting UserId
-    $userData = doValenceRequest('GET', '/d2l/api/lp/' . $config['LP_Version'] . '/users/?userName=' . $userName);
-    if ($userData['Code']==200){
+    //course offering properties, see https://docs.valence.desire2learn.com/res/course.html#Course.CreateCourseOffering for more deatils
+    $courseParameters = array("Name"             => $siteName,
+                              "Code"             => $siteCode,
+                              "Path"             => '',
+                              "CourseTemplateId" => $siteTemplate,
+                              "SemesterId"       => null,
+                              "StartDate"        => null,
+                              "EndDate"          => null,
+                              "LocaleId"         => null,
+                              "ForceLocale"      => false,
+                              "ShowAddressBook"  => false,
+                              "Description"      => array("Content"=>$siteName, "Type"=>"Html"),
+                              "CanSelfRegister"  => null);
+    
+    //create course offering    
+    $createOffering = doValenceRequest('POST','/d2l/api/lp/' . $config['LP_Version'] . '/courses/', $courseParameters);
+    
+    //if course offering created successfully, then add user to the site as Instructor
+    if ($createOffering['Code']==200){
+        $userData = doValenceRequest('GET', '/d2l/api/lp/' . $config['LP_Version'] . '/users/?userName=' . $userName);
         $userId = $userData['response']->UserId;
-        //$postOfferingData = array("OrgUnitId"=>(int)$orgUnitId,"UserId"=>$userId,"RoleId"=>(int)$_POST[userrole]);
-        //$offerringEnroll = doValenceRequest('POST', '/d2l/api/lp/'. $config['LP_Version'] .'/enrollments/', $postOfferingData);
-        echo json_encode('UserId' . $userId);
+        $postOfferingData = array("OrgUnitId"=>$createOffering['response']->Identifier,"UserId"=>$userId,"RoleId"=>109);
+        $offerringEnroll = doValenceRequest('POST', '/d2l/api/lp/'. $config['LP_Version'] .'/enrollments/', $postOfferingData);
     }
-    else{
-        echo json_encode("No such user");
+    else {
+        echo "Something went wrong while creating a course offering";
         return;
     }
     
+    //Send back to js success code and new OrgUnitId    
+    if ($offerringEnroll['Code']==200){
+        $results = array("Code" => $offerringEnroll['Code'],
+                         "OrgUnitId" => $offerringEnroll['response']->Identifier);
+        echo json_encode($results);
+    }
+    else{
+        echo "Something went wrong while creating a course offering"
+    }
 }
 else {
         echo "User has no permission to create a site";
